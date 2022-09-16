168fd22dcd6d0f97cfd7bf19427f6147c7bc2033
dariszka <darinapetrova05@gmail.com>
2022-09-16 14:36:11 +0200

    Change names.

--- excel-json.js
+++ excel-json.js
@@ -6,7 +6,6 @@ const wb = xlsx.readFile("./Data.xlsx")
 const ws = wb.Sheets["Sheet1"]
 const data = xlsx.utils.sheet_to_json(ws);
 
-let fullMatchesArray = []
 
 match()
 
@@ -14,8 +13,10 @@ function match() {
     let names = data.map(x => x.name);
     let spokenLs = data.map(x => x.spokenL);
     let desiredLs = data.map(x => x.desiredL);
-
+    
     let noMatchesArray = names
+    let partialMatchesArray
+    let fullMatchesArray = []
 
     for (let i = 0; i < data.length; i++) {
 
@@ -35,26 +36,26 @@ function match() {
                         (element != firstLoopPerson)&&(element!=secondLoopPerson))
                 } 
             }
-    } readAllResults(noMatchesArray)
+    } readAllResults(fullMatchesArray, noMatchesArray)
 }
     
 
-function readAllResults(finalNonMatches) {
-    fullMatchesArray.forEach((pair) => { 	
+function readAllResults(fullMatches, nonMatches) {
+    fullMatches.forEach((pair) => { 	
         pair.sort()
     });
-        fullMatchesArray = fullMatchesArray.filter(function(element) {
+        fullMatches = fullMatches.filter(function(element) {
             return element !== undefined;
         });
 
-    for (let i = 0; i < fullMatchesArray.length; i++) {
-        let stringPair = fullMatchesArray[i].toString();
+    for (let i = 0; i < fullMatches.length; i++) {
+        let stringPair = fullMatches[i].toString();
         let formattedStringPair = stringPair.replace(","," and ");
-        fullMatchesArray[i] = formattedStringPair
+        fullMatches[i] = formattedStringPair
     }
-    let finalFullMatches =  [...new Set(fullMatchesArray)];
+    let finalFullMatches =  [...new Set(fullMatches)];
 
-    finalNonMatches = finalNonMatches.toString().replace(/,/g, 'n')
+    let finalNonMatches = nonMatches.toString().replace(/,/g, '\n')
     
 
 
@@ -64,8 +65,8 @@ function readAllResults(finalNonMatches) {
 function displayResults(fullMatches, nonMatches) {
     let formattedFullMatches = 'Full matches: \n' + fullMatches.toString().replace(/,/g,'\n') 
     //let formattedPartialMatches = '\n' + 'No full matches were found for: \n' + partialMatches + ' is a partial match'
-    let formattedNonMatches = '\n' + 'No matches were found for: \n' + nonMatches.toString()
+    let formattedNonMatches = '\n' + 'No matches were found for: \n' + nonMatches
     
     let textResult =  formattedFullMatches + formattedNonMatches
-    //console.log(textResult)
+    console.log(textResult)
 }
